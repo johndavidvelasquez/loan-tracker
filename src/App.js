b549ca0b52e791e04072a1efb73bdf939523fbed
johndavidvelasquez <johndavidvelaquezz@gmail.com>
2021-03-03 21:42:07 +0800

    Initial Design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ChakraProvider, extendTheme, Box } from "@chakra-ui/react";
+import Home from "./Components/Home/Home";
+import Header from "./Components/Header/Header";
 
-function App() {
+const theme = extendTheme({
+  config: {
+    useSystemColorMode: true,
+    initialColorMode: "dark",
+  },
+});
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider theme={theme}>
+      <Header />
+      <Box maxW="lg" m={2} pt={10} className="baseContainer">
+        <Home />
+      </Box>
+      {/* <div className="baseContainer">
+     borderWidth="1px" borderRadius="lg"
+      </div> */}
+    </ChakraProvider>
   );
-}
+};
 
 export default App;
